0bcf0cd3b5b7add6e5ced1d2e0f796a41af4bbb2
shpakova <shpakovaanastasiia@gmail.com>
2021-01-08 14:04:08 +0200

    12345

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-a = 3
-while a < 10:
+def function (a):
+ while a < 10:
     a = a + 2
     if a > 10:
         print('дождались!', a)
     elif a < 10:
         print(a, "пока что нет")
+function (3)
