caaceb6618844bd43d403a90d305af75445a84cf
shpakova <shpakovaanastasiia@gmail.com>
2021-01-18 19:28:51 +0200

    11

--- Common member.py
+++ Common member.py
@@ -1,5 +1,8 @@
 def common (a,b):
     return list(set(a) & set(b))
-a = [7,6,5,4,6,8,9]
-b = [9,9,8,7,7]
-print(common (a,b))
+import random
+a = [random.randrange(1, 50, 1) for i in range(20)]
+b = [random.randrange(1, 50, 1) for i in range(20)]
+print(a)
+print(b)
+print(common(a, b))
