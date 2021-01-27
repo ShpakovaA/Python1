7e25b2dbe00961b73b8c10e9c235e7a3a46a108e
shpakova <shpakovaanastasiia@gmail.com>
2021-01-27 20:39:51 +0200

    16

--- высокосный год.py
+++ высокосный год.py
@@ -3,5 +3,5 @@ def leap_year(year):
     if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
         print('Это высокосный год')
     else:
-        print('Это обычный год')
+        print('Это обычный год ')
 leap_year(year)
